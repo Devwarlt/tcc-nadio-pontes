67fff8511b89a03c60e218489d08ef77b4be25ea
Devwarlt <nadio.pontes@redes.unb.br>
2021-11-18 15:48:53 -0300

    Create __init__.py

---
+++ src/mocks/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from typing import *
+from random import *
+
+
+def get_random_report() -> Dict[str, Any]:
+    pass
+
+def get_random_incident_foresight() -> Dict[str, Any]:
+    pass
