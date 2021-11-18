568051566f9c3b7a207f61b7127d9244efb941c0
Devwarlt <nadio.pontes@redes.unb.br>
2021-11-18 15:48:56 -0300

    Create schema.sql

---
+++ src/schema.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS `sisbin`
+	DEFAULT CHARACTER SET latin1
+	COLLATE latin1_swedish_ci;
+
+USE `sisbin`;
+
+CREATE TABLE IF NOT EXISTS `report` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`date` DATETIME NOT NULL,
+	`raw_data` MEDIUMBLOB NOT NULL
+	PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `prediction_history` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`date` DATETIME NOT NULL,
+	`incident_foresight` FLOAT NOT NULL
+	PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
