0d6e5358debd282803bd73c81350dbf6dca2cf99
Devwarlt <nadio.pontes@redes.unb.br>
2024-05-14 22:43:15 -0300

    Update __init__.py

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -1,18 +1,22 @@
 from datetime import datetime, timedelta
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-from mock import MockUtils
+from forecast import IncidentForesight
+from settings import Settings
 
 root_router: APIRouter = APIRouter()
 
 # FastAPI - Declare Request Example Data:
 # https://fastapi.tiangolo.com/tutorial/schema-extra-example/
 
+# Carga e Geração - ONS
+# https://www.ons.org.br/paginas/energia-agora/carga-e-geracao
+
 
 @root_router.get(
     "/incident_foresight",
     description="""
-        Retrives all incident foresight predictions between 'start_period (required)' and 'end_period'
+        Retrives all incident foresight predictions between 'start_period (required)' and 'final_period'
         (default: if not set, then API will calculate next upcoming 7 days since 'start_period' as
         reference).
         """,
@@ -20,11 +24,13 @@ root_router: APIRouter = APIRouter()
 )
 def get_incident_foresight_callback(
     start_period: datetime = datetime.today().date(),
-    end_period: datetime | None = None,
+    final_period: datetime | None = None,
 ) -> JSONResponse:
-    if not end_period:
-        end_period = start_period + timedelta(days=7)
+    if not final_period or final_period <= start_period:
+        final_period = start_period + timedelta(days=7)
 
-    return JSONResponse(
-        MockUtils.get_random_incident_foresight_results(start_period, end_period)
+    incident_foresight: IncidentForesight = IncidentForesight(
+        start_period, final_period, **Settings.CONFIG["forecast"]
     )
+    incident_foresight.predict()
+    return JSONResponse(list(incident_foresight.serialize_forecasts()))
